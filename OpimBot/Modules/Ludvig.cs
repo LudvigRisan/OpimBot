7e43c4324f2d6307fb2a33145f0b7642b6d752b9
LudvigRisan <ludvig.risan@gmail.com>
2018-02-07 15:20:40 +0100

    legg til ludvig sin class

---
+++ OpimBot/Modules/Ludvig.cs
@@ -0,0 +1,40 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpimBot.Modules {
+    public class Ludvig : ModuleBase<SocketCommandContext> {
+
+        Random randomizer = new Random();
+
+        [Command("ludvig.rng")]
+        public async Task Rng() {
+
+            int randInt = (randomizer.Next() % 100) + 1;
+
+            await ReplyAsync(randInt.ToString());
+
+        }
+
+        [Command("ludvig.rng")]
+        public async Task Rng(int max) {
+
+            int randInt = (randomizer.Next() % max) + 1;
+
+            await ReplyAsync(randInt.ToString());
+
+        }
+
+        [Command("ludvig.rng")]
+        public async Task Rng(int min, int max) {
+
+            int randInt = (randomizer.Next() % (max - min)) + min;
+
+            await ReplyAsync(randInt.ToString());
+
+        }
+    }
+}
