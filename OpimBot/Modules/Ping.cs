af790de0f9d040e58ba4c129263bdef4cda09217
LudvigRisan <ludvig.risan@gmail.com>
2018-02-07 11:15:53 +0100

    Hello world
    Let's hope this works

---
+++ OpimBot/Modules/Ping.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpimBot.Modules {
+    public class Ping : ModuleBase<SocketCommandContext> {
+
+        
+
+        [Command("!ping")]
+        public async Task pingAsync() {
+
+            await ReplyAsync("Pong!");
+
+        }
+
+        [Command("!boop")]
+        public async Task boopAsync() {
+
+            await ReplyAsync("Noooooooooo!");
+
+        }
+
+        [Command("!bounce")]
+        public async Task bounceTaskAsync(string message) {
+
+            await ReplyAsync(message);
+
+        }
+
+        [Command("!Asshole")]
+        public async Task assholeAsync() {
+            
+            await ReplyAsync($"{Context.User.Mention} is a useless piece of waste!");
+
+        }
+
+        [Command("!embed")]
+        public async Task EmbedifyAsync() {
+
+            EmbedBuilder embedder = new EmbedBuilder();
+
+            embedder.AddField("Some user:", Context.User.Mention)
+                .AddInlineField("Best bot:", Context.Client.CurrentUser.Mention)
+                .AddInlineField("Best user:", "<@213297762844934144>");
+
+            await ReplyAsync("", false, embedder.Build());
+
+        }
+
+        
+
+    }
+}
---
+++ OpimBot/Program.cs
@@ -0,0 +1,81 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpimBot {
+
+    class Program {
+
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync() {
+
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
+
+            string botToken = "NDA4OTM4NzEwODU2NjMwMjg0.DVshoQ.eWxqLarTXgv9r0mwDX6v43chkks";
+
+            //logging
+
+            _client.Log += log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task log(LogMessage arg) {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync() {
+
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg) {
+
+            var message = arg as SocketUserMessage;
+
+            if(message == null || message.Author.IsBot) {
+                return;
+            }
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)) {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess) {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+
+        }
+    }
+
+}
